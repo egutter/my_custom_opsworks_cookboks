33e507e7e65310baff643e79d102a94124c17024
Alejandro Pulver <alepulver@gmail.com>
2014-02-03 13:59:04 -0300

    Change unicorn restart for passenger

--- opsworks_custom_env/recipes/restart_command.rb
+++ opsworks_custom_env/recipes/restart_command.rb
@@ -2,7 +2,8 @@ node[:deploy].each do |application, deploy|
 
   execute "restart Rails app #{application} for custom env" do
     cwd deploy[:current_path]
-    command "#{deploy[:deploy_to]}/shared/scripts/unicorn restart"
+    #command "#{deploy[:deploy_to]}/shared/scripts/unicorn restart"
+    command "touch /srv/www/lawvault/current/tmp/restart.txt"
     user deploy[:user]
 
     action :nothing
