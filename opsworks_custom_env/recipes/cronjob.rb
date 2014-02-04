df9090b63035c5b80417567e520737e2ff96454a
Alejandro Pulver <alejandro@8thlight.com>
2014-02-04 11:07:51 -0300

    Add cron jobs

--- opsworks_custom_env/recipes/cronjob.rb
+++ opsworks_custom_env/recipes/cronjob.rb
@@ -1,6 +1,15 @@
-cron "job_name" do
-  hour "*"
-  minute "10"
-  weekday "*"
-  command "cd $HOME && date >> mylog.txt"
+tasks = [
+	{name: 'clean:unconfirmed_documents', hour: '6', minute: '0'},
+	{name: 'reminders:send_document_deadlines', hour: '6', minute: '30'},
+	{name: 'reminders:send_discovery_end_date', hour: '7', minute: '0'},
+	{name: 'reminders:scheduled_events', hour: '7', minute: '30'},
+]
+
+tasks.each do |a_task|
+	cron a_task[:name] do
+	  hour a_task[:hour]
+	  minute a_task[:minute]
+	  weekday "*"
+	  command "cd #{deploy[:deploy_to]}/current && bundle exec rake #{a_task[:name]}"
+	end
 end
--- opsworks_custom_env/recipes/restart_command.rb
+++ opsworks_custom_env/recipes/restart_command.rb
@@ -2,7 +2,6 @@ node[:deploy].each do |application, deploy|
 
   execute "restart Rails app #{application} for custom env" do
     cwd deploy[:current_path]
-    #command "#{deploy[:deploy_to]}/shared/scripts/unicorn restart"
     command "touch #{deploy[:deploy_to]}/current/tmp/restart.txt"
     user deploy[:user]
 
