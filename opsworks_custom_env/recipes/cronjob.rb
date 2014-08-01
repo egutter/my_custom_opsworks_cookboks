4475fcccac0faaa926d3c457bb256bf50a5a02a7
Emilio Gutter <egutter@gmail.com>
2014-08-01 14:29:27 -0300

    Add pending_invitation reminder

--- opsworks_custom_env/recipes/cronjob.rb
+++ opsworks_custom_env/recipes/cronjob.rb
@@ -3,6 +3,7 @@ tasks = [
 	{:name => 'reminders:send_document_deadlines', :hour => '6', :minute => '30'},
 	{:name => 'reminders:send_discovery_end_date', :hour => '7', :minute => '0'},
 	{:name => 'reminders:scheduled_events', :hour => '7', :minute => '30'},
+	{:name => 'reminders:pending_invitation', :hour => '8', :minute => '00'},
 ]
 
 tasks.each do |a_task|
@@ -10,6 +11,6 @@ tasks.each do |a_task|
 	  hour a_task[:hour]
 	  minute a_task[:minute]
 	  weekday "*"
-	  command "cd /srv/www/lawvault/current && bundle exec rake #{a_task[:name]} RAILS_ENV=performance"
+	  command "cd /srv/www/lawvault/current && bundle exec rake #{a_task[:name]} RAILS_ENV=production"
 	end
 end
